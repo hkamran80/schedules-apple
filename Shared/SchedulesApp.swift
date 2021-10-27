e2bf6599ab2d6cf2b763833ac110542d3a1287ff
Zheng <aheze@getfind.app>
2021-10-27 14:12:09 -0700

    Fix height too short

--- Shared/SchedulesApp.swift
+++ Shared/SchedulesApp.swift
@@ -11,8 +11,12 @@ import SwiftUI
 struct SchedulesApp: App {
     var body: some Scene {
         WindowGroup {
+#if os(macOS)
             ContentView()
                 .frame(minWidth: 450, idealWidth: 500, maxWidth: .infinity, minHeight: 250, idealHeight: 250, maxHeight: 500, alignment: .center)
+#elseif os(iOS)
+//            ContentView()
+#endif
         }
     }
 }
